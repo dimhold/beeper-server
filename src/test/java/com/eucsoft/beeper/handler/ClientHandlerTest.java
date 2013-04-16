f46424fb6dadef3554265a2cca14825e85c2c17e
dimhold <dimhold@gmail.com>
2013-04-16 14:18:35 +0300

    Add onMessageBegin test and Add clientHandlers map to Beeper

--- src/test/java/com/eucsoft/beeper/handler/ClientHandlerTest.java
+++ src/test/java/com/eucsoft/beeper/handler/ClientHandlerTest.java
@@ -1,8 +1,5 @@
 package com.eucsoft.beeper.handler;
 
-import java.util.HashSet;
-
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,29 +11,28 @@ import com.eucsoft.beeper.model.User;
 
 @PrepareForTest(MainTest.class)
 public class ClientHandlerTest {
-	
+
 	@Test
 	public void onConnect() {
 		ClientHandler clientHandler = new ClientHandler();
 		clientHandler.onConnect(new User(), "some user information");
-		
+
 		int expected = 1;
 		int actual = Beeper.getInstance().getUsers().size();
 		Assert.assertEquals(actual, expected);
 	}
-	
+
 	@Test
 	public void onConnect_user_has_device_info() {
 		String expected = "some user information";
-		
+
 		ClientHandler clientHandler = new ClientHandler();
 		clientHandler.onConnect(new User(), expected);
-		HashSet<User> hs = new HashSet<User>();
-		
+
 		User user = Beeper.getInstance().getUsers().iterator().next();
-		
+
 		String actual = user.getDeviceInfo();
-		
+
 		Assert.assertEquals(actual, expected);
 	}
 
@@ -49,7 +45,7 @@ public class ClientHandlerTest {
 		int actual = Beeper.getInstance().getBench().size();
 		Assert.assertEquals(actual, expected);
 	}
-	
+
 	@Test
 	public void onMessageBegin(User user) {
 		ClientHandler clientHandler = new ClientHandler();
@@ -60,35 +56,36 @@ public class ClientHandlerTest {
 		user2.setRoom(room);
 		room.addUser(user1);
 		room.addUser(user2);
-		
-		clientHandler.onMessageBegin(user1);
+
+		ClientHandler clientHandler2 = Beeper.getInstance().getClientHandler(user2);
+
+		// Check that clientHandler2.sendMessageBigein is triggered;
 	}
-	
+
 	@Test
 	public void onMessage(byte[] message, User user) {
 	}
-	
+
 	@Test
 	public void onMessageEnd(User user) {
 	}
-	
+
 	@Test
 	public void onDisconnect(User user) {
 	}
-	
-	
+
 	@Test
 	public void sendMessageBegin(User user) {
 	}
-	
+
 	@Test
 	public void sendMessage(byte[] message) {
 	}
-	
+
 	@Test
 	public void sendMessageEnd(User user) {
 	}
-	
+
 	@Test
 	public void sendChangeRoom(User user) {
 	}
