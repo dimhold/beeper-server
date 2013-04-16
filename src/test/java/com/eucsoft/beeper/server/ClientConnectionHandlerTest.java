dda13962cba23cc98b0c00991d5d628fa49ead7d
viktar-umg <viktar@upmygame.com>
2013-04-17 01:27:46 +0300

    Add ClientConnectionHandlerTest stab

---
+++ src/test/java/com/eucsoft/beeper/server/ClientConnectionHandlerTest.java
@@ -0,0 +1,11 @@
+package com.eucsoft.beeper.server;
+
+import static org.powermock.api.mockito.PowerMockito.*;
+
+public class ClientConnectionHandlerTest {
+	
+	public void inboundBufferUpdated() {
+		ClientConnectionHandler handler = mock(ClientConnectionHandler.class);
+		
+	}
+}
