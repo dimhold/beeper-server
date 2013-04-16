35073d5be1eafe87ad82cbf86f7f4f5c04c8bcae
dimhold <dimhold@gmail.com>
2013-04-16 11:56:56 +0300

    Add first test case example

--- src/test/java/com/eucsoft/beeper/MainTest.java
+++ src/test/java/com/eucsoft/beeper/MainTest.java
@@ -1,12 +1,26 @@
 package com.eucsoft.beeper;
 
-
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@PrepareForTest(MainTest.class)
 public class MainTest {
+	
+	public String test() {
+		return "work";
+	}
 
 	@Test
-	public void testApp() {
-		System.out.println("Power mouck and testng");
+	public void demoStaticMethodMocking() throws Exception {
+		String expected = "successful";
+		MainTest main = PowerMockito.mock(MainTest.class);
+		PowerMockito.when(main.test()).thenReturn(expected);
+		
+		String actual = main.test();
+
+		Assert.assertEquals(actual, expected);
 	}
+
 }
