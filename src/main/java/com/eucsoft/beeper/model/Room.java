a3faeb06b7a426a0dbf08cc467e67e4944d0a8e4
by-ktif <monovar@tut.by>
2013-04-16 06:11:59 -0430

    Room and User objects implemented

--- src/main/java/com/eucsoft/beeper/model/Room.java
+++ src/main/java/com/eucsoft/beeper/model/Room.java
@@ -1,5 +1,22 @@
 package com.eucsoft.beeper.model;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 public class Room {
 
+	private Collection<User> users = new HashSet<User>();
+	
+	public Collection<User> getUsers() {
+		return users;
+	}
+	
+	public void addUser(User user) {
+		users.add(user);
+	}
+	
+	public void removeUser(User user) {
+		users.remove(user);
+	}
+	
 }
--- src/main/java/com/eucsoft/beeper/model/User.java
+++ src/main/java/com/eucsoft/beeper/model/User.java
@@ -2,4 +2,50 @@ package com.eucsoft.beeper.model;
 
 public class User {
 
+	private String deviceInfo;
+	
+	private Room room;
+	
+	public String getDeviceInfo() {
+		return deviceInfo;
+	}
+
+	public void setDeviceInfo(String deviceInfo) {
+		this.deviceInfo = deviceInfo;
+	}
+	
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((deviceInfo == null) ? 0 : deviceInfo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (deviceInfo == null) {
+			if (other.deviceInfo != null)
+				return false;
+		} else if (!deviceInfo.equals(other.deviceInfo))
+			return false;
+		return true;
+	}
+	
 }
