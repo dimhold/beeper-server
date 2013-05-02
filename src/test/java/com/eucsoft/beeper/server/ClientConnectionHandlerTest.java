1528cb8f51e0c3b3da26c8842c2d7666887c8e0c
viktar-umg <viktar@upmygame.com>
2013-05-02 21:23:33 +0300

    playing with tests

--- src/test/java/com/eucsoft/beeper/server/ClientConnectionHandlerTest.java
+++ src/test/java/com/eucsoft/beeper/server/ClientConnectionHandlerTest.java
@@ -2,28 +2,30 @@ package com.eucsoft.beeper.server;
 
 //import static org.powermock.api.mockito.PowerMockito.*;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import io.netty.buffer.ByteBuf;
 
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.testng.annotations.Test;
 
 import com.eucsoft.beeper.handler.ClientHandler;
-import com.eucsoft.beeper.model.User;
 @PrepareForTest(ClientHandler.class)
 public class ClientConnectionHandlerTest {
 	@Test
-	public void inboundBufferUpdated() throws Exception {
-		User user = mock(User.class);
+	public void inboundBufferUpdatedTest() throws Exception {
 		ClientHandler clientHandler = mock(ClientHandler.class);
 		PowerMockito.whenNew(ClientHandler.class).withNoArguments().thenReturn(clientHandler);
-		ClientConnectionHandler handler = new ClientConnectionHandler(user);
-		ByteBuf in = mock(ByteBuf.class);
 		
+		ClientConnectionHandler handler = new ClientConnectionHandler();
+		PowerMockito.verifyNew(ClientHandler.class).withNoArguments();
+		ByteBuf in = mock(ByteBuf.class);
+		Mockito.when(in.array()).thenReturn(null);
 		handler.inboundBufferUpdated(null, in);
-		//ClientHandler clientHandler = Beeper.getInstance().getClientHandler(user);
-		verify(clientHandler, times(1)).onMessageBegin();
+		//verify(clientHandler, times(1)).onConnect("");
 		
 	}
 }
