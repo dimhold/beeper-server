4661a90f753b52f9ffd516e46d79e27065247f44
viktar-umg <viktar@upmygame.com>
2013-05-02 22:27:07 +0300

    inboundBufferUpdate test stab

--- src/test/java/com/eucsoft/beeper/server/ClientConnectionHandlerTest.java
+++ src/test/java/com/eucsoft/beeper/server/ClientConnectionHandlerTest.java
@@ -3,29 +3,52 @@ package com.eucsoft.beeper.server;
 //import static org.powermock.api.mockito.PowerMockito.*;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 import io.netty.buffer.ByteBuf;
 
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.eucsoft.beeper.handler.ClientHandler;
-@PrepareForTest(ClientHandler.class)
-public class ClientConnectionHandlerTest {
+import com.eucsoft.beeper.server.command.Command;
+import com.eucsoft.beeper.server.command.CommandType;
+
+@PrepareForTest(ClientConnectionHandler.class)
+public class ClientConnectionHandlerTest extends PowerMockTestCase {
 	@Test
 	public void inboundBufferUpdatedTest() throws Exception {
 		ClientHandler clientHandler = mock(ClientHandler.class);
-		PowerMockito.whenNew(ClientHandler.class).withNoArguments().thenReturn(clientHandler);
-		
-		ClientConnectionHandler handler = new ClientConnectionHandler();
-		PowerMockito.verifyNew(ClientHandler.class).withNoArguments();
+		PowerMockito.whenNew(ClientHandler.class).withNoArguments()
+				.thenReturn(clientHandler);
+		ClientConnectionHandler handler = mock(ClientConnectionHandler.class);
+		//PowerMockito.verifyNew(ClientHandler.class).withNoArguments();
+
+		for (CommandType commandType : CommandType.values()) {
+			Command command = new Command();
+			command.setType(commandType);
+			String testData = "testData";
+			command.setData(testData.getBytes());
+			PowerMockito.verifyPrivate(handler).invoke("dispatchCommand",command);
+			
+			switch (commandType) {
+			case CONNECT:
+				verify(clientHandler, times(1)).onConnect(testData);
+				break;
+			// case
+			default:
+				Assert.assertTrue(false);
+				break;
+			}
+		}
+
 		ByteBuf in = mock(ByteBuf.class);
 		Mockito.when(in.array()).thenReturn(null);
 		handler.inboundBufferUpdated(null, in);
-		//verify(clientHandler, times(1)).onConnect("");
-		
+
 	}
 }
